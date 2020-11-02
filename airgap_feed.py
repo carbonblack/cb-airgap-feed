c921f6677319bd125c7e5f77c511f241d3c56dd3
Dave Seidel <dseidel@carbonblack.com>
2020-11-02 20:16:23 +0000

    CB-31410: Python 3 and general code cleanup, fixed some bugs

--- airgap_feed.py
+++ airgap_feed.py
@@ -1,119 +1,158 @@
+#!/usr/bin/env python
+
+import argparse
 import json
-import logging
 import os
-
-import cbapi.example_helpers as cbhelper
+import re
 import requests
-from cbapi.response import *
+import shutil
+import sys
+from typing import Dict, Optional, List
 
-log = logging.getLogger(__name__)
-
-#
-# The MIT License (MIT)
-#
-# Copyright (c) 2014-2020 VMware, Inc. All Rights Reserved.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# -----------------------------------------------------------------------------
-#
-# Zachary Estep 2017
+# noinspection PyBroadException
+try:
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning, InsecurePlatformWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+except Exception:
+    pass
 
 
-class CbFeedShipper(object):
-    def __init__(self, cbapi=None, force_override=None, verbose=None):
-        self.cbapi = cbapi
-        self.force_override = force_override
-        self.verbose = True if verbose else False
+def get_api_token() -> str:
+    import psycopg2
+    conn = psycopg2.connect("dbname=cb port=5002")
+    cur = conn.cursor()
+    cur.execute("select auth_token from cb_user where global_admin is true order by id limit 1;")
+    token = cur.fetchone()[0]
+    token = re.sub("^'|'$", "", token)
+    return token
 
-    def import_folder(self, folder):
-        # Feed queries seem to be broken
-        feeds = {feed.name: feed for feed in self.cbapi.select(Feed).all()}
-        print(feeds)
-        for root, subdirs, files in os.walk(folder):
-            for file in files:
-                filepath = os.path.join(root, file)
-                feed_url = "file://%s" % filepath
-                filejson = json.loads(open(filepath).read())
-                feedname = filejson['feedinfo']['name']
-                feed = feeds.get(feedname, None)
-                if feed is not None and not self.force_override:
-                    print("Skipping... {} (already existed)".format(feedname))
-                else:
-                    # create a feed if one didn't exist
-                    feed = feed if feed else self.cbapi.create(Feed)
-                    feed.name = feedname
-                    feed.feed_url = feed_url
-                    feed.validate_server_cert = False
-                    feed.manually_added = True
-                    feed.enabled = True
-                    feed.save()
-                    feed.refresh()
-                    print(feed)
 
-    def export_feeds(self, outdir, cert=None):
+def get_feed(headers: Dict, name: str) -> Optional[Dict]:
+    # if the port is something other than 443 change below
+    url = "https://127.0.0.1:443/api/v1/feed"
+    feeds = requests.get(url, headers=headers, verify=False)
+    feeds.raise_for_status()
+    for feed in feeds.json():
+        if feed['name'].lower() == name.lower():
+            return feed
+    return None 
 
-        log.info("export_feeds: outdir = {} , cert = {}".format(outdir, cert))
 
-        if cert is None:
-            cert = ("/etc/cb/carbonblack-alliance-client.crt", "/etc/cb/carbonblack-alliance-client.key")
+def build_cli_parser(description: str = "VMware Carbon Black EDR Airgap Feeds import/export utility") \
+        -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description=description)
+    return parser
+
+
+def main(argv: List) -> int:
+    parser = build_cli_parser()
+    commands = parser.add_subparsers(help="Commands", dest="command")
+
+    default_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "feeds")
+
+    import_command = commands.add_parser("import", help="Import feeds from disk")
+    import_command.add_argument("-f", "--folder", help="Folder to import", default=default_folder, required=False)
+
+    export_command = commands.add_parser("export", help="Export feeds to disk")
+    export_command.add_argument("-f", "--folder", help="Folder to export to", default=None, required=True)
+
+    args = parser.parse_args(args=argv)
+
+    mode = args.command
+
+    if hasattr(args, "folder"):
+        folder = args.folder
+    else:
+        # use location of script
+        folder = os.path.dirname(os.path.abspath(__file__))
+
+    header = {'X-Auth-Token': get_api_token()}
+
+    # if the port is something other than 443 change below
+    url = "https://127.0.0.1:443/api/v1/feed"
+
+    errors = 0
+    if mode == 'import':
+        print(f'Importing Threat Intelligence feeds from {folder}')
+
+        for root, sub_dirs, files in os.walk(folder):
+            for temp_file in files:
+                if temp_file.endswith('.json'):
+                    filepath = os.path.join(root, temp_file)
+                    print(f'filepath = {filepath}')
+
+                    feed_url = f"file://{filepath}"
+                    data = {'feed_url': feed_url,
+                            'validate_server_cert': False,
+                            'manually_added': True}
+
+                    file_json = json.loads(open(filepath).read())
+                    feed_name = file_json['feedinfo']['name']
+                    feed = get_feed(header, feed_name)
+                    if feed is not None:
+                        feed_id = feed['id']
+                        print(f"Feed {feed_name} already exists, attempting update")
+                        feed.update({"feed_url": feed_url, "manually_added": True})
+                        feed_update = requests.put(f"{url}/{feed_id}", data=json.dumps(feed), headers=header,
+                                                   verify=False)
+                        if feed_update.status_code == 200:
+                            print(f"Updated {feed_name}")
+                        else:
+                            print(f"Failed to update {feed_name} (error {feed_update.status_code})")
+                            errors += 1
+                    else:
+                        feed_update = requests.post(url, data=json.dumps(data), headers=header, verify=False)
+                        if feed_update.status_code == 200:
+                            print(f"Added feed {feed_name}")
+                        else:
+                            print(f"Failed to add feed {feed_name} (error {feed_update.status_code})")
+                            errors += 1
+    else:
+        export_path = os.path.join(folder, "feeds")
+        print(f'Exporting Threat Intelligence Feeds to {export_path}')
+
+        try:
+            os.makedirs(export_path)
+        except OSError:
+            pass  # probably due to folder already existing
+
+        try:
+            shutil.copy(os.path.abspath(__file__), folder)
+        except shutil.SameFileError:
+            pass
+
+        cert = ("/etc/cb/certs/carbonblack-alliance-client.crt",
+                "/etc/cb/certs/carbonblack-alliance-client.key")
 
         export_feeds = ['abusech', 'Bit9AdvancedThreats', 'alienvault',
                         'CbCommunity', 'Bit9EarlyAccess', 'Bit9SuspiciousIndicators', 'Bit9EndpointVisibility',
-                        'fbthreatexchange', 'iconmatching', 'CbKnownIOCs', 'sans', 'mdl', 'ThreatConnect', 'tor', 'attackframework']
+                        'fbthreatexchange', 'CbKnownIOCs', 'sans', 'mdl', 'ThreatConnect', 'tor', 'attackframework']
 
-        export_feeds = map(lambda f: self.cbapi.select(Feed).where("name:" + f).first(), export_feeds)
-        export_feeds = filter(lambda f: not f.feed_url.startswith("file:/"), export_feeds)
+        feeds = requests.get(url, headers=header, verify=False)
+        feeds.raise_for_status()
 
-        for feed in export_feeds:
-            response = requests.get(url=feed.feed_url,cert=cert).json()
-            log.debug(response)
-            log.info("Trying to export feed {} , url {} ".format(feed.name, feed.feed_url))
-            file = open(os.path.join(outdir, feed.name + ".json"), "w+")
-            file.write(json.dumps(response))
+        for feed in feeds.json():
+            feed_url = feed.get('feed_url', None)
+            feed_name = str(feed.get('name', ""))
+            print(f"Checking feed {feed_name} at {feed_url}")
+            if feed_url and 'http' in feed_url and feed_name in export_feeds:
+                try:
+                    response = requests.get(url=feed_url, cert=cert)
+                    response.raise_for_status()
+                    fn = os.path.join(export_path, feed_name + ".json")
+                    f = open(fn, "w+")
+                    try:
+                        print(f"Exporting feed {feed_name} to {fn}")
+                        f.write(json.dumps(response.json()))
+                    except Exception as e:
+                        print(f'Error writing to {feed_name}: {e}')
+                        errors += 1
+                except Exception as e:
+                    print(f'Could not export feed {feed_name}: {e}')
+                    errors += 1
 
-
-def main():
-    parser = cbhelper.build_cli_parser()
-    parser.add_argument("--folder", dest="folder",
-                        help="Input Folder where Cb Threat Intelligence Feed JSON files are stored", required=True)
-    parser.add_argument("--force-override", dest="override", help="Force update of existing feeds", required=False,
-                        default=False)
-    parser.add_argument("--mode", dest="mode", help="import or export", required=True)
-    parser.add_argument("--cert", dest="cert", help="cert",required=False,default=None)
-    parser.add_argument("--key", dest="key",help="key",required=False,default=None)
-
-    args = parser.parse_args()
-
-    cbapi = cbhelper.get_cb_response_object(args)
-
-    shipper = CbFeedShipper(cbapi=cbapi, force_override=args.override, verbose=args.verbose)
-
-    print("[+] Processing folder: {}\n".format(args.folder))
-
-    if args.mode == "export":
-        shipper.export_feeds(outdir=args.folder,cert=(args.cert,args.key) if args.cert and args.key else None)
-    else:
-        shipper.import_folder(folder=args.folder)
+    return 0 if not errors else 1
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.DEBUG)
-    main()
-
+    sys.exit(main(sys.argv[1:]))
